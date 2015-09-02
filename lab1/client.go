60897213f2d0e18b00129377d79cfcd06d4223a2
Evgeny Petrov <fiddenmar@gmail.com>
2015-09-02 20:14:11 +0300

    initial commit

---
+++ lab1/client.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func CheckError(err error) {
+	if err  != nil {
+		fmt.Println("Error: " , err)
+	}
+}
+
+type Client struct {
+	sendIP string
+	sendPort int
+	answerPort int
+	login string
+	Answers chan string
+}
+
+func main() {
+	var client Client
+	client.Init("User", "127.0.0.1", 34310)
+	go client.Answer()
+	client.Connect()
+	client.Send("Hello message")
+}
+
+func (client *Client) Init(_login string, _sendIP string, _sendPort int) {
+	client.sendPort = _sendPort
+	client.answerPort = client.sendPort + 1
+	client.sendIP = _sendIP
+	client.login = _login
+	client.Answers = make(chan string)
+}
+
+func (client *Client) Connect() {
+	client.Send(client.login)
+}
+
+func (client *Client) Send(message string) {
+	ServerAddr,err := net.ResolveUDPAddr("udp", client.sendIP+":"+strconv.Itoa(client.sendPort))
+	CheckError(err)
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	CheckError(err)
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	CheckError(err)
+	defer Conn.Close()
+	buf := []byte(message)
+	_, err = Conn.Write(buf)
+	CheckError(err)
+}
+
+func (client *Client) Answer() (message string) {
+	AnswerAddr,err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(client.answerPort))
+    CheckError(err)
+    AnswerConn, err := net.ListenUDP("udp", AnswerAddr)
+    CheckError(err)
+    defer AnswerConn.Close()
+    buf := make([]byte, 1024)
+
+    for {
+	    n, _, err := AnswerConn.ReadFromUDP(buf)
+	    CheckError(err)
+	    ans := string(buf[0:n])
+	    client.Answers <- ans
+	}
+}
---
+++ lab1/server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "time"
+    "os"
+    "strconv"
+)
+
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+        os.Exit(0)
+    }
+}
+
+type Server struct {
+	listenPort int
+	broadcastPort int
+	userList map[string]string //ip->username
+	messages chan string
+}
+
+func main() {   
+	var server Server
+	server.Init(34310)
+	go server.Broadcast()
+	server.Listen()
+	for {
+
+	}
+}
+
+func (server *Server) Init(_listenPort int) {
+	server.listenPort = _listenPort
+	server.broadcastPort = server.listenPort + 1
+	server.userList = nil
+	server.userList = make(map[string]string)
+	server.messages = make(chan string)
+}
+
+func (server *Server) Listen() {
+	ServerAddr,err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(server.listenPort))
+    CheckError(err)
+    ServerConn, err := net.ListenUDP("udp", ServerAddr)
+    CheckError(err)
+    defer ServerConn.Close()
+    buf := make([]byte, 1024)
+
+    for {
+        n,addr,err := ServerConn.ReadFromUDP(buf)
+        CheckError(err)
+        if n > 0 {
+	        var message string
+	        if _, ok := server.userList[addr.IP.String()]; ok {
+	        	fmt.Println("Received ", string(buf[0:n]), " from ", addr, " at ", time.Now())
+	        	message = server.userList[addr.IP.String()] + " said at " + time.Now().String() + ": " + string(buf[0:n])
+	        } else {
+	        	server.userList[addr.IP.String()] = string(buf[0:n])
+	        	message = "User " + string(buf[0:n]) + " (" + addr.IP.String() + ") joined at " + time.Now().String()
+	        	fmt.Println(message)
+	        }
+	        server.messages <- message
+	    }
+    }
+}
+
+func (server *Server) Broadcast() {
+	for {
+		msg := <- server.messages
+		for ip, _ := range server.userList {
+			RecvAddr,err := net.ResolveUDPAddr("udp", ip+":"+strconv.Itoa(server.broadcastPort))
+			CheckError(err)
+			LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+			CheckError(err)
+			Conn, err := net.DialUDP("udp", LocalAddr, RecvAddr)
+			CheckError(err)
+			defer Conn.Close()
+			buf := []byte(msg)
+			_, err = Conn.Write(buf)
+			CheckError(err);
+		}
+	}
+}
